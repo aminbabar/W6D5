8b6f27b26c8ee2601eee5effefc6eccd87d2c3ee
aminbabar <amin.babar1@gmail.com>
2021-07-23 15:01:08 -0400

    show and index views working

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -1,16 +1,19 @@
 class CatsController < ApplicationController
 
     def index
-      
         @cats = Cat.all 
         render 'index'
-        
-
     end
 
+
     def show
-    
-    
+
+        @cat = Cat.find_by(id: params[:id])
+        if @cat
+            render :show
+        else
+            redirect_to cats_url
+        end
     end
 
 
