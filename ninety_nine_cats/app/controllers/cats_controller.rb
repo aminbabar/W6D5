41f7c10b174201dc7e5fd80b258d25beaa547c3d
Michael Amicucci <mikeamicucci34@gmail.com>
2021-07-23 11:44:03 -0700

    cats name

---
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,18 @@
+class CatsController < ApplicationController
+
+    def index
+      
+        @cats = Cat.all 
+        render 'index'
+        
+
+    end
+
+    def show
+    
+    
+    end
+
+
+
+end
--- ninety_nine_cats/app/models/cat.rb
+++ ninety_nine_cats/app/models/cat.rb
@@ -1,10 +1,12 @@
 require 'action_view'
 
 COLORS = %w(black gold pink purple turquoise green scarlet)
+SEX = ['M', 'F']
 
 class Cat < ApplicationRecord
     
     validates :color, inclusion: {in: COLORS, message: "%{value} is not a valid cat color"}
+    validates :sex, inclusion: {in: SEX, message: "%{value} is not a valid sex"}
     
     
     def age(birth_date)
