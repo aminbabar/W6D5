3ece241c57faeb4533fda22dc1092add072e8f2a
Michael Amicucci <mikeamicucci34@gmail.com>
2021-07-23 12:30:24 -0700

    cats seed

--- ninety_nine_cats/db/seeds.rb
+++ ninety_nine_cats/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+
+day = (1..28).to_a
+month = (1..12).to_a
+year = (1900..2020).to_a
+
+COLORS = %w(black gold pink purple turquoise green scarlet)
+
+# cat = Cat.create(name: 'bob', birth_date: '2020/2/4', color: 'gold', sex: 'F', description: 'this is a nice cat')
+
+100.times do 
+    cat = Cat.new
+    cat.name = Faker::FunnyName.name
+    # debugger
+    cat.birth_date = "#{year.sample}/#{month.sample}/#{day.sample}"
+    cat.color = COLORS.sample
+    cat.sex = ['M', 'F'].sample
+    cat.description = Faker::Hipster.sentence
+    cat.save!
+end
